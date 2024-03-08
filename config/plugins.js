61070f2269c8f44391a301d862732a2d4338d7df
metalclaw <zeratul11@hotmail.com>
2024-03-08 12:27:07 +0100

    Added Bunny.net and Amazon SES plugins
    - added Bummy.net and Amazon SES plugins to project
    - added new Amazon SES and Bunny.net keys to .env.example
    - updated plugins and middlewares configs with required code for both plugins to work

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,26 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+	upload: {
+		config: {
+		  provider: "strapi-provider-upload-bunnynet",
+		  providerOptions: {
+			api_key: process.env.BUNNY_API_KEY,
+			storage_zone: process.env.BUNNY_STORAGE_ZONE,
+			pull_zone: process.env.BUNNY_PULL_ZONE,
+		  },
+		},
+	},	
+	email: {
+		config: {
+		  provider: 'amazon-ses',
+		  providerOptions: {
+			key: env('AWS_SES_KEY'),
+			secret: env('AWS_SES_SECRET'),
+			amazon: 'https://email.us-east-1.amazonaws.com',
+		  },
+		  settings: {
+			defaultFrom: 'myemail@protonmail.com',
+			defaultReplyTo: 'myemail@protonmail.com',
+		  },
+		},
+	},
+});
